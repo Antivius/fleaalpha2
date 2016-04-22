149b09549a4a5421965733560e6ccdd5a9e96c9b
bkamau <bkamau@abo.fi>
2016-04-22 19:00:36 +0300

    initial

--- app/src/main/java/fi/benson/views/fragments/Search.java
+++ app/src/main/java/fi/benson/views/fragments/Search.java
@@ -1,11 +1,25 @@
 package fi.benson.views.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fi.benson.R;
 
 /**
  * Created by bkamau on 04/04/16.
  */
 public class Search extends Fragment {
 
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+
+        return rootView;
+
+    }
 
 }
